006f8694b48f0e85c78151f9e3a6f757041d3125
Douglas Santos <doug-cg@hotmail.com>
2025-02-16 14:04:13 -0300

    adicoiona sistema para conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+        Scanner ContaPrincipal = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o numero da Conta:");
+        int numeroConta = ContaPrincipal.nextInt();
+
+
+        System.out.println("Digite o número da agência:");
+        String numeroAgencia = ContaPrincipal.next();
+
+        System.out.println("Digite o nome de Usuário:");
+        String nomeConta = ContaPrincipal.next();
+
+        System.out.println("Digita o seu Saldo:");
+        double saldoAtual = ContaPrincipal.nextDouble();
+
+        System.out.println("Olá " + nomeConta + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + ", conta " + numeroConta + "e seu saldo " + saldoAtual + " já está disponível para saque.");
+
+        ContaPrincipal.close();
+
     }
 }
