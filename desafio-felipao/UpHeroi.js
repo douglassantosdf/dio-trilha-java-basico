0f7b666c877bdd6ac5f0d243b33f30e7b258ca4c
douglassantosdf <doug-cg@hotmail.com>
2025-04-19 10:39:36 -0300

    add desafio felipao

--- desafio-felipao/UpHeroi.js
+++ desafio-felipao/UpHeroi.js
@@ -1,13 +1,34 @@
 // declaracao heroi
 
 let nomeHeroi = "Doug"
-let nivelHeroi = 0
+let xpHeroi = 12000
+let nivelHeroi = ""
 
-switch (nivelHeroi){
-     case (nivelHeroi < 1000):
-        nivelHeroi = FileSystemDirectoryReader;
-        break
-
+if (xpHeroi <=1000){
+    nivelHeroi = "Ferro"
 }
+if (xpHeroi > 1000 && xpHeroi <= 2000){
+    nivelHeroi = "Bronze"
+}
+if (xpHeroi > 2000 && xpHeroi <= 5000){
+    nivelHeroi = "Prata"
+}
+if (xpHeroi > 5000 && xpHeroi <= 7000){
+    nivelHeroi = "Ouro"
+}
+if (xpHeroi > 7000 && xpHeroi <= 8000){
+    nivelHeroi = "Platina"
+}
+if (xpHeroi > 8000 && xpHeroi <= 9000){
+    nivelHeroi = "Ascendente"
+}
+if (xpHeroi > 9000 && xpHeroi <= 10000){
+    nivelHeroi = "Imortal"
+}
+if (xpHeroi >= 10001){
+    nivelHeroi = "Radiante"
+}
+
+
 
 console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivelHeroi)
