3c96b2a74577b4bd9ffac2b5b4940ce9a7df048b
douglassantosdf <doug-cg@hotmail.com>
2025-04-19 10:43:13 -0300

    update

--- desafio-felipao/UpHeroi.js
+++ desafio-felipao/UpHeroi.js
@@ -4,28 +4,21 @@ let nomeHeroi = "Doug"
 let xpHeroi = 12000
 let nivelHeroi = ""
 
-if (xpHeroi <=1000){
+if (xpHeroi <= 1000) {
     nivelHeroi = "Ferro"
-}
-if (xpHeroi > 1000 && xpHeroi <= 2000){
+} else if (xpHeroi <= 2000) {
     nivelHeroi = "Bronze"
-}
-if (xpHeroi > 2000 && xpHeroi <= 5000){
+} else if (xpHeroi <= 5000) {
     nivelHeroi = "Prata"
-}
-if (xpHeroi > 5000 && xpHeroi <= 7000){
+} else if (xpHeroi <= 7000) {
     nivelHeroi = "Ouro"
-}
-if (xpHeroi > 7000 && xpHeroi <= 8000){
+} else if (xpHeroi <= 8000) {
     nivelHeroi = "Platina"
-}
-if (xpHeroi > 8000 && xpHeroi <= 9000){
+} else if (xpHeroi <= 9000) {
     nivelHeroi = "Ascendente"
-}
-if (xpHeroi > 9000 && xpHeroi <= 10000){
+} else if (xpHeroi <= 10000) {
     nivelHeroi = "Imortal"
-}
-if (xpHeroi >= 10001){
+} else {
     nivelHeroi = "Radiante"
 }
 
