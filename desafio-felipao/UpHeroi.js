3abdd3e3ee3aa9ea5378fddb1c16f34b2babae2c
Douglas Santos <66652006+douglassantosdf@users.noreply.github.com>
2025-04-18 23:56:43 -0300

    criando pasta desafio  felipao

---
+++ desafio-felipao/UpHeroi.js
@@ -0,0 +1,13 @@
+// declaracao heroi
+
+let nomeHeroi = "Doug"
+let nivelHeroi = 0
+
+switch (nivelHeroi){
+     case (nivelHeroi < 1000):
+        nivelHeroi = FileSystemDirectoryReader;
+        break
+
+}
+
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivelHeroi)
